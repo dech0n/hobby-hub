5e4c38c61cba2593a3eb599a891297de10e221ba
Dechon Ryan <dechon@tuta.io>
2021-06-01 12:33:28 -0700

    Add comment to test merge and pull request

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+// import created session secret from index.js config file
 const { sessionSecret } = require('./config/index');
 
 const app = express();
