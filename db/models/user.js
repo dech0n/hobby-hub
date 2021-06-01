1d50523f1021e78cba3c61a40e3dd791dcadee3d
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-06-01 14:17:02 -0600

    Create model and migration files for User and Wheelhouse

---
+++ db/models/user.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    hashedPassword: {
+      allowNull: false,
+      type: DataTypes.STRING.BINARY
+    }
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
