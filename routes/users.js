93c9c131a31a25c02c1118a9dcbabdf10530e541
J0914 <jordynsechrist@gmail.com>
2021-06-01 17:34:44 -0500

    add auth.js, utils,js import and export both

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { loginUser, logoutUser } = require('../auth.js');
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils.js');
+const { check, validationResult } = require('express-validator');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
