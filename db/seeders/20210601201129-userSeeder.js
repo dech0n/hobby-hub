69f79124080d01e890eaaa063a8a48dbbf65209b
J0914 <jordynsechrist@gmail.com>
2021-06-02 11:02:00 -0500

    Update users and wheelhouse seeds

--- db/seeders/20210601201129-userSeeder.js
+++ db/seeders/20210601201129-userSeeder.js
@@ -2,6 +2,13 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {firstName: 'Demo', lastName: 'User', username: 'Demo', email: 'demo@user.com', hashedPassword: '$2a$10$VrnuQDHjCdjw9/c3SRPL/.DIJIpLglyX3ZX.OhgNDQYGKlXKGC8mO', createdAt: new Date(), updatedAt: new Date() },
+      {firstName: 'Bradley', lastName: 'Cooper', username: 'ChrisKyle', email: 'bcoop@snipes.com', hashedPassword: '$2a$10$VrnuQDHjCdjw9/c3SRPL/.DIJIpLglyX3ZX.OhgNDQYGKlXKGC8mO', createdAt: new Date(), updatedAt: new Date() },
+      {firstName: 'Jennifer', lastName: 'Lopez', username: 'JennyFromThaBlock', email: 'jenny@popstar.com', hashedPassword: '$2a$10$VrnuQDHjCdjw9/c3SRPL/.DIJIpLglyX3ZX.OhgNDQYGKlXKGC8mO', createdAt: new Date(), updatedAt: new Date() },
+      {firstName: 'Johnny', lastName: 'Depp', username: 'CaptainJackSparrow', email: 'black@pearl.com', hashedPassword: '$2a$10$VrnuQDHjCdjw9/c3SRPL/.DIJIpLglyX3ZX.OhgNDQYGKlXKGC8mO', createdAt: new Date(), updatedAt: new Date() },
+      {firstName: 'Robert', lastName: 'Pattinson', username: 'Sparkles', email: 'psychoboyfriend@vegetarian.com', hashedPassword: '$2a$10$VrnuQDHjCdjw9/c3SRPL/.DIJIpLglyX3ZX.OhgNDQYGKlXKGC8mO', createdAt: new Date(), updatedAt: new Date() },
+    ])
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -15,6 +22,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {})
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
