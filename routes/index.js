e47e5bb74330b9c7a97d6e4a57370cb122f32f91
J0914 <jordynsechrist@gmail.com>
2021-06-02 11:31:51 -0500

    Debug index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hobby Hub Home' });
+  const user = req.session.user;
+  res.render('index', { title: 'Hobby Hub Home', });
 });
 
 module.exports = router;
