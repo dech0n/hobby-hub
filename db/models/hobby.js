af686f4cf175fbda961ea3a53a466a9f7cae64d5
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-06-02 12:18:20 -0600

    Add hobby seeds, create /hobbies/:hobbyId path

--- db/models/hobby.js
+++ db/models/hobby.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const Hobby = sequelize.define('Hobby', {
     title: DataTypes.STRING,
-    description: DataTypes.TEXT
+    description: DataTypes.TEXT,
+    img: DataTypes.STRING
   }, {});
   Hobby.associate = function(models) {
     // associations can be defined here
--- routes/hobbies.js
+++ routes/hobbies.js
@@ -1,17 +1,27 @@
-const express = require('express');
-const db = require('../db/models');
-const { asyncHandler } = require('./utils.js');
-const { check, validationResult } = require('express-validator');
+const express = require("express");
+const db = require("../db/models");
+const { asyncHandler } = require("./utils.js");
+const { check, validationResult } = require("express-validator");
 
 const router = express.Router();
 
-router.get('/', asyncHandler(async (req, res) => {
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
     const hobbies = await db.Hobby.findAll();
-    res.render('hobbies', {
-        title: 'Hobbies',
-        hobbies,
-    })
-}))
+    res.render("hobbies", {
+      title: "Hobbies",
+      hobbies,
+    });
+  })
+);
 
+router.get("/:hobbyId(\\d+)", asyncHandler(async(req, res) => {
+    const hobbyId = parseInt(req.params.hobbyId, 10);
+    const hobby = await db.Hobby.findByPk(hobbyId, { include: db.Experience });
+    console.log(hobby);
+
+    res.render('hobby', { title: `Hobby: ${hobby.title}`, hobby });
+}));
 
 module.exports = router;
