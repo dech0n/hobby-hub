07143d8f3efef737bf9c72dfe946f0f6367653a7
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-06-02 11:14:35 -0600

    Create splash page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('DID WE GET HERE?')
   res.render('index', { title: 'Hobby Hub Home' });
 });
 
