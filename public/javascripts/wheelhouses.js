57842fa7d862497eba59ae4cd979172867021ba7
J0914 <jordynsechrist@gmail.com>
2021-06-03 00:06:25 -0500

    Update api for wheelhouses

--- public/javascripts/wheelhouses.js
+++ public/javascripts/wheelhouses.js
@@ -1,8 +1,13 @@
-window.addEventListener('load', async () => {
-    
+const wheelhouseBtns = document.querySelectorAll('.wheelhouseLink')
+
+const allBtn = document.getElementById('all');
+
+allBtn.addEventListener('click', async () => {
+    allBtn.className = 'wheelhouseLink active'
+
     try {
         const res = await fetch('/api/all');
-        const wheelhouses = await res.json();
+        wheelhouses = await res.json();
         const hobbiesDiv = document.querySelector('.hobbies')
         let userHobbies = []
         wheelhouses.forEach(wheelhouse => {
@@ -14,11 +19,69 @@ window.addEventListener('load', async () => {
                 hobbies.push(ele.Hobby)
             })
         })
-        
-        console.log(hobbies)
+
+        hobbies.forEach(hobby => {
+            const hobbyDiv = document.createElement('div')
+            hobbyDiv.setAttribute('class', 'hobbies__content');
+            hobbyDiv.setAttribute('href', `/hobbies/${hobby.id}`)
+            hobbiesDiv.appendChild(hobbyDiv);
+
+            const link = document.createElement('a')
+            link.setAttribute('href', `/hobbies/${hobby.id}`)
+            link.setAttribute('class', 'hobby_links')
+            link.innerHTML = `${hobby.title}`
+            hobbyDiv.appendChild(link);
+
+            const img = document.createElement('img')
+            img.setAttribute('src', `${hobby.img}`)
+            img.setAttribute('alt', 'illustration of the hobby')
+            img.setAttribute('class', 'hobby_image');
+            hobbyDiv.appendChild(img);
+
+            const title = document.createElement('p')
+            title.setAttribute('class', 'hobby_title');
+            title.innerHTML = `${hobby.title}`;
+            hobbyDiv.appendChild(title);
+            
+            const description = document.createElement('p')
+            description.setAttribute('class', 'hobby_description');
+            description.innerHTML = `${hobby.description}`;
+            hobbyDiv.appendChild(description);
+        })
     } catch (e) {
         console.log(e)
     }
 })
+
+const wantToLearnBtn = document.getElementById('Want to Learn');
+
+wantToLearnBtn.addEventListener('click', async () => {
+    wantToLearnBtn.className = 'wheelhouseLink active'
+
+    try {
+        const res = await fetch('/api/wantToLearn');
+        wheelhouse = await res.json();
+        const hobbiesDiv = document.querySelector('.hobbies')
+        let userHobbies = wheelhouse.UserHobbies
+
+    console.log(userHobbies)  
+    } catch (e) {
+        console.log('whoops')
+    }      
+})
+
+const currentlyLearningBtn = document.getElementById('Currently Learning');
+
+currentlyLearningBtn.addEventListener('click', () => {
+    currentlyLearningBtn.className = 'wheelhouseLink active'
+})
+
+const accomplishedBtn = document.getElementById('Accomplished');
+
+accomplishedBtn.addEventListener('click', () => {
+    accomplishedBtn.className = 'wheelhouseLink active'
+})
+
+
 
 
