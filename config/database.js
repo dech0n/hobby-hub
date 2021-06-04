8a63fa965f6a0126ca4c0003a0184a0f63719ed5
Dechon Ryan <dechon@tuta.io>
2021-06-03 17:05:43 -0700

    Update config file for Heroku

--- config/database.js
+++ config/database.js
@@ -11,4 +11,9 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize',
   },
+  production: {
+    use_env_variable: 'postgres://uackesgeqxtowm:70b9b3e34beac1e369c85dde1b871ddaa2753a8ae0cd654d73c8a1052eccfeda@ec2-52-0-114-209.compute-1.amazonaws.com:5432/ddk24b5toco12i',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  }
 };
