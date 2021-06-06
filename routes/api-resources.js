c81d866b97e8015c0275b1f0e01b5ece04e0b6ae
J0914 <jordynsechrist@gmail.com>
2021-06-05 20:43:58 -0500

    Update resources feature

--- routes/api-resources.js
+++ routes/api-resources.js
@@ -5,43 +5,38 @@ const { asyncHandler } = require('./utils');
 const router = express.Router();
 
 // get all resources of a given hobby
-
-const getResources = async (hobbyId) => {
-    return await db.Resource.findAll({where: { hobbyId }})
-}
-
 router.get(
     "/hobbies/:hobbyId",
     asyncHandler(async (req, res) => {
         const userId = req.session.auth.userId;
         const hobbyId = req.params.hobbyId;
         const resources = await db.Resource.findAll({
-            where: {
-                hobbyId
-            }
+            where: [{hobbyId},{userId}] 
         })
-        console.log('GOT HERE----------', resources)
       res.json({ resources });
     })
 );
 
-// // post a new resource
-// router.post(
-//     "/hobbies/:hobbyId",
-//     asyncHandler(async (req, res) => {
-//       const userId = req.session.auth.userId;
-//       const hobbyId = req.params.hobbyId;
-//       const {title, link} = req.body.message;
-//       console.log(title, link);
-//       const resource = await db.Resource.create({
-//           hobbyId,
-//           userId,
-//           title,
-//           link
-//       });
-//       res.json({ resource });
-//     })
-// );
+// post a new resource
+router.post(
+    "/hobbies/:hobbyId",
+    asyncHandler(async (req, res) => {
+      const userId = req.session.auth.userId;
+      const hobbyId = req.params.hobbyId;
+      const {title, link} = req.body;
+      await db.Resource.create({
+          hobbyId,
+          userId,
+          title,
+          link
+      });
+      console.log('Got Here ------------------------------')
+      const resources = await db.Resource.findAll({
+        where: [{hobbyId},{userId}] 
+    })
+      res.json({ resources });
+    })
+);
 
 //   // delete a given resource
 // router.delete(
